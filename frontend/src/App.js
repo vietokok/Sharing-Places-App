69d6a8b1cb4ed5685b94d47f5c8d71151e802005
vietokok <khacviet201299@gmail.com>
2020-04-28 08:34:36 +0700

    Creat default frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
+import './App.css';
+
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<Users />
+				</Route>
+				<Route path="/places/new" exact>
+					<NewPlace />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		</Router>
+	);
+};
+
+export default App;
