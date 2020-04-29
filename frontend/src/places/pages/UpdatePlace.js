e8ee97575cf4badc097fd5a2c9b45f3b54050d9a
vietokok <khacviet201299@gmail.com>
2020-04-29 16:51:22 +0700

    Create add new place and update place

---
+++ frontend/src/places/pages/UpdatePlace.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+	VALIDATOR_REQUIRE,
+	VALIDATOR_MINLENGTH,
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+
+import './PlaceForm.css';
+
+const DUMMY_PLACES = [
+	{
+		id: 'p1',
+		title: 'Empire State Building',
+		description: 'One of most famous sky scrapers in the world!',
+		imageUrl: 'https://picsum.photos/200/300',
+		address: '20 W 34th St, New Y',
+		location: {
+			lat: 40.7484405,
+			lng: -73.9878531,
+		},
+		creator: 'u1',
+	},
+	{
+		id: 'p2',
+		title: 'Bình Trù',
+		description: 'My village!',
+		imageUrl: 'https://picsum.photos/200/300',
+		address: 'Dương Quang Gia Lâm Hanoi',
+		location: {
+			lat: 21.0109844,
+			lng: 105.9874034,
+		},
+		creator: 'u2',
+	},
+];
+
+const UpdatePlace = () => {
+	const placeId = useParams().placeId;
+
+	const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+	const [formState, inputHandler] = useForm(
+		{
+			title: {
+				value: identifiedPlace.title,
+				isValid: true,
+			},
+			description: {
+				value: identifiedPlace.description,
+				isValid: true,
+			},
+		},
+		true
+	);
+
+	const placeUpdateSubmitHandler = (event) => {
+		event.preventDefault();
+		console.log(formState.inputs);
+	};
+
+	if (!identifiedPlace) {
+		return (
+			<div className="center">
+				<h2>Could not find place!</h2>
+			</div>
+		);
+	}
+
+	return (
+		<form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+			<Input
+				id="title"
+				element="input"
+				type="text"
+				label="Title"
+				validators={[VALIDATOR_REQUIRE()]}
+				errorText="Please enter a valid title."
+				onInput={inputHandler}
+				initialValue={formState.inputs.title.value}
+				initialValid={formState.inputs.title.isValid}
+			/>
+			<Input
+				id="description"
+				element="textarea"
+				label="Description"
+				validators={[VALIDATOR_MINLENGTH(5)]}
+				errorText="Please enter a valid description (min 5 characters)."
+				onInput={inputHandler}
+				initialValue={formState.inputs.description.value}
+				initialValid={formState.inputs.description.isValid}
+			/>
+			<Button type="submit" disabled={!formState.isValid}>
+				UPDATE PLACE
+			</Button>
+		</form>
+	);
+};
+
+export default UpdatePlace;
