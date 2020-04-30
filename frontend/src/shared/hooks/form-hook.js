b17a0c893e509d6039ad3f1008283ab998fdfb08
vietokok <khacviet201299@gmail.com>
2020-04-30 16:15:07 +0700

    Add login interface

--- frontend/src/shared/hooks/form-hook.js
+++ frontend/src/shared/hooks/form-hook.js
@@ -5,6 +5,10 @@ const formReducer = (state, action) => {
 		case 'INPUT_CHANGE':
 			let formIsValid = true;
 			for (const inputId in state.inputs) {
+				// if field not exist
+				if (!state.inputs[inputId]) {
+					continue;
+				}
 				// if key in data equals inputId of action function
 				if (inputId === action.inputId) {
 					formIsValid = formIsValid && action.isValid;
