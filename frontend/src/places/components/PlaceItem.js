8f611ad3420ca6c8a921926d2683144f43614168
vietokok <khacviet201299@gmail.com>
2020-04-30 17:07:34 +0700

    using useContext to authenticate and finished frontend

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceItem.css';
 
 // render list places by user id
 const PlaceItem = (props) => {
+	const auth = useContext(AuthContext);
 	const [showMap, setShowMap] = useState(false);
 	const [showConfirmModal, setShowConfirmModal] = useState(false);
 
@@ -78,10 +80,15 @@ const PlaceItem = (props) => {
 						<Button inverse onClick={openMapHandler}>
 							VIEW ON MAP
 						</Button>
-						<Button to={`/places/${props.id}`}>EDIT</Button>
-						<Button danger onClick={showDeleteHandler}>
-							DELETE
-						</Button>
+						{auth.isLoggedIn && (
+							<Button to={`/places/${props.id}`}>EDIT</Button>
+						)}
+
+						{auth.isLoggedIn && (
+							<Button danger onClick={showDeleteHandler}>
+								DELETE
+							</Button>
+						)}
 					</div>
 				</Card>
 			</li>
--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 // the links in navigation
 const NavLinks = (props) => {
+	const auth = useContext(AuthContext);
+
 	return (
 		<ul className="nav-links">
 			<li>
@@ -12,15 +15,26 @@ const NavLinks = (props) => {
 					ALL USERS
 				</NavLink>
 			</li>
-			<li>
-				<NavLink to="/u1/places">MY PLACES</NavLink>
-			</li>
-			<li>
-				<NavLink to="/places/new">ADD PLACES</NavLink>
-			</li>
-			<li>
-				<NavLink to="/auth">AUTHENTICATE</NavLink>
-			</li>
+			{auth.isLoggedIn && (
+				<li>
+					<NavLink to="/u1/places">MY PLACES</NavLink>
+				</li>
+			)}
+			{auth.isLoggedIn && (
+				<li>
+					<NavLink to="/places/new">ADD PLACES</NavLink>
+				</li>
+			)}
+			{!auth.isLoggedIn && (
+				<li>
+					<NavLink to="/auth">AUTHENTICATE</NavLink>
+				</li>
+			)}
+			{auth.isLoggedIn && (
+				<li>
+					<button onClick={auth.logout}>LOGOUT</button>
+				</li>
+			)}
 		</ul>
 	);
 };
