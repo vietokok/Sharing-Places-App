cb7d800fc065fd93c8847d0e18110058496091f5
vietokok <khacviet201299@gmail.com>
2020-05-09 09:18:58 +0700

    POST to signup

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+	next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -29,7 +39,7 @@ app.use((error, req, res, next) => {
 
 mongoose
 	.connect(
-		'mongodb+srv://vietokok:ThuHuyen2111@cluster0-5w2ys.gcp.mongodb.net/places?retryWrites=true&w=majority'
+		'mongodb+srv://vietokok:ThuHuyen2111@cluster0-5w2ys.gcp.mongodb.net/mern?retryWrites=true&w=majority'
 	)
 	.then(() => {
 		app.listen(5000);
--- frontend/src/shared/hooks/form-hook.js
+++ frontend/src/shared/hooks/form-hook.js
@@ -5,21 +5,17 @@ const formReducer = (state, action) => {
 		case 'INPUT_CHANGE':
 			let formIsValid = true;
 			for (const inputId in state.inputs) {
-				// if field not exist
 				if (!state.inputs[inputId]) {
 					continue;
 				}
-				// if key in data equals inputId of action function
 				if (inputId === action.inputId) {
 					formIsValid = formIsValid && action.isValid;
 				} else {
 					formIsValid = formIsValid && state.inputs[inputId].isValid;
 				}
 			}
-
 			return {
 				...state,
-				// new inputs data
 				inputs: {
 					...state.inputs,
 					[action.inputId]: { value: action.value, isValid: action.isValid },
@@ -31,20 +27,17 @@ const formReducer = (state, action) => {
 				inputs: action.inputs,
 				isValid: action.formIsValid,
 			};
-
 		default:
 			return state;
 	}
 };
 
-export const useForm = (initalInputs, initalFormValidity) => {
-	// args[0]: data, args[1]: function
+export const useForm = (initialInputs, initialFormValidity) => {
 	const [formState, dispatch] = useReducer(formReducer, {
-		inputs: initalInputs,
-		isValid: initalFormValidity,
+		inputs: initialInputs,
+		isValid: initialFormValidity,
 	});
 
-	// useCallBack to against render many times
 	const inputHandler = useCallback((id, value, isValid) => {
 		dispatch({
 			type: 'INPUT_CHANGE',
